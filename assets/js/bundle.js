5107142bd715407de490c5842d62151690ce076e
ffalt <ffalty@gmail.com>
2016-05-04 13:06:04 +0200

    bundle js files

---
+++ assets/js/bundle.js
@@ -0,0 +1,8 @@
+---
+---
+{% include js/jquery.min.js %}
+{% include js/jquery.easing.min.js %}
+{% include js/jquery.magnific-popup.min.js %}
+{% include js/bootstrap.min.js %}
+{% include js/slick.min.js %}
+{% include js/index.js %}
